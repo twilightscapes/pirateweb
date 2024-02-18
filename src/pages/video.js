dc229db2ecb3d95a390c9a9b0dcdf569c825af89
Todd <ihatespam@gmail.com>
2024-02-18 01:41:52 -0600

    video RC1

--- src/pages/video.js
+++ src/pages/video.js
@@ -1,38 +1,61 @@
 import React from "react";
-
 import PageMenu from "../components/PageMenu";
 import Layout from "../components/siteLayout";
 import Seo from "../components/seo";
 import { Helmet } from "react-helmet";
 import useSiteMetadata from "../hooks/SiteMetadata";
 import VideoPlayer from "../components/VideoPlayer";
-const VideoPage = ({ location }) => {
 
+const VideoPage = ({ location }) => {
   const { proOptions } = useSiteMetadata();
   const { showBranding } = proOptions;
-  
-      return (
-        <Layout>
-          <Helmet>
-            <body id="body" className="youtube"/>
-          </Helmet>
-          <Seo
-            title="Pirate Video Player"
-            description="Pirate Video Player"
-          />
-          <>
-          <div className='player-wrapper'>
-            {showBranding ? (
-              <PageMenu />
-            ) : (
-              ""
-            )}
-<VideoPlayer location={location} />
-</div>
-    
-    </>
-    </Layout>
 
+  return (
+    <Layout>
+      <Helmet>
+        <body id="body" className="youtube" />
+      </Helmet>
+      <SeoWrapper location={location} />
+      <div className='player-wrapper'>
+        {showBranding ? (
+          <PageMenu />
+        ) : (
+          null
+        )}
+        <VideoPlayer location={location} />
+      </div>
+    </Layout>
+  );
+};
+
+const SeoWrapper = ({ location }) => {
+  const queryParams = new URLSearchParams(location.search);
+  const videoUrlParam = queryParams.get('video');
+
+  const extractVideoId = (url) => {
+    // Check if the URL is null or undefined
+    if (!url) {
+      return null;
+    }
+
+    // Regular expression to extract video ID from YouTube URL
+    const regExp = /^(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
+    const match = url.match(regExp);
+
+    // If a match is found, return the video ID, otherwise return null
+    const videoId = match ? match[1] : null;
+    console.log('Video ID:', videoId);
+    return videoId;
+  };
+
+  const videoId = extractVideoId(videoUrlParam);
+
+  return (
+    <Seo
+      title="Pirate Video Player"
+      description="Pirate Video Player"
+      image={videoId ? `https://i.ytimg.com/vi/${videoId}/hqdefault.jpg` : null}
+    />
   );
 };
 
