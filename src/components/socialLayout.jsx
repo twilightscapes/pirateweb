969e46d159d6171754eb0623914fba86502ce667
Todd <ihatespam@gmail.com>
2024-02-18 11:14:56 -0600

    breakfix

--- src/components/socialLayout.jsx
+++ src/components/socialLayout.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import Seo from "./seo"
 // import { Link } from 'gatsby-plugin-modal-routing-4'
+import { Link } from "gatsby"
 import "../styles/reset.css"
 import "../styles/global.css"
-import { AnchorLink, Link } from "gatsby-plugin-anchor-links"
+import { AnchorLink } from "gatsby-plugin-anchor-links"
 // import { navigate } from "gatsby"
 import { Helmet } from "react-helmet"
 import Theme from "./theme"
