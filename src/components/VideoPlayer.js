49f973b8827e3b0bde4b024b01f789e248e6f425
Todd <ihatespam@gmail.com>
2024-02-22 07:02:01 -0600

    Update VideoPlayer.js

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -273,13 +273,13 @@ const VideoPlayer = ({ location }) => {
                 <form className="youtubeform1 frontdrop1" onSubmit={handleSubmit} id="youtubeform" name="youtubeform" style={{display:'flex', justifyContent:'center', flexWrap:'wrap', alignItems:'center', width:'100vw', margin:'0 auto', gap:'2vw', padding:'1vh 2vw' }}>
 
 
-<div id="bigbox" style={{ display: 'flex', flexFlow:'wrap', flexDirection:'', gap: '4px', alignItems: 'center', width:'100%', border:'0px solid red' }}>
+<div id="bigbox" style={{ display: 'flex', flexFlow:'wrap', flexDirection:'', gap: '2vw', alignItems: 'center', width:'', border:'0px solid red' }}>
 
 
 <div id="controls" style={{ display: 'flex', flexDirection:'row', gap: '2vw', alignItems: 'center', width:'' }}>
 
 <div id="checkboxes" style={{ display: 'flex', flexDirection:'row', gap: '1.5vw', alignItems: 'center' }}>
-                                <label htmlFor="loop-checkbox" style={{textAlign:'center', fontSize:'60%', display:'flex', flexDirection:'column'}}>Loop:
+                                <label htmlFor="loop-checkbox" style={{textAlign:'center', fontSize:'60%', display:'flex', flexDirection:'column', alignItems:'center'}}>Loop:
                                     <input
                                         aria-label="Set to loop"
                                         id="loop-checkbox"
@@ -291,7 +291,7 @@ const VideoPlayer = ({ location }) => {
                                         style={{maxWidth:'50px'}}
                                     />
                                 </label>
-                                <label htmlFor="mute-checkbox" style={{textAlign:'center', fontSize:'60%', display:'flex', flexDirection:'column'}}>Mute:
+                                <label htmlFor="mute-checkbox" style={{textAlign:'center', fontSize:'60%', display:'flex', flexDirection:'column', alignItems:'center'}}>Mute:
                                     <input
                                         aria-label="Set to mute"
                                         id="mute-checkbox"
@@ -305,7 +305,7 @@ const VideoPlayer = ({ location }) => {
                                 </label>
 
                                 
-                                <label htmlFor="controls-checkbox" style={{textAlign:'center', fontSize:'50%', display:'flex', flexDirection:'column'}}>Controls:
+                                <label htmlFor="controls-checkbox" style={{textAlign:'center', fontSize:'50%', display:'flex', flexDirection:'column', alignItems:'center'}}>Controls:
                                     <input
                                         aria-label="Set to show controls"
                                         id="controls-checkbox"
@@ -319,7 +319,7 @@ const VideoPlayer = ({ location }) => {
                                 </label>
 
                             
-<label htmlFor="blocker-checkbox" style={{textAlign:'center', fontSize:'60%', display:'flex', flexDirection:'column'}}>Block
+                                <label htmlFor="blocker-checkbox" style={{textAlign:'center', fontSize:'60%', display:'flex', flexDirection:'column', alignItems:'center'}}>Block:
     <input
         aria-label="Block user interactions"
         id="blocker-checkbox"
@@ -327,16 +327,18 @@ const VideoPlayer = ({ location }) => {
         name="showBlocker"
         checked={showBlocker}
         onChange={handleBlockerChange}
+        disabled={!isVideoActive}
         style={{maxWidth:'50px'}}
     />
 </label>
 
-<label htmlFor="autoplayCheckbox" style={{textAlign:'center', fontSize:'50%', display:'flex', flexDirection:'column'}}>Autoplay
+<label htmlFor="autoplayCheckbox" style={{textAlign:'center', fontSize:'50%', display:'flex', flexDirection:'column', alignItems:'center'}}>Autoplay:
     <input
         type="checkbox"
         id="autoplayCheckbox"
         checked={autoplay}
         onChange={(e) => setAutoplay(e.target.checked)}
+        disabled={!isVideoActive}
     />
 </label>
 
@@ -377,7 +379,7 @@ const VideoPlayer = ({ location }) => {
 </div>
 
 
-<div id="pastebox" style={{ display: 'flex', flexDirection:'row', gap: '10px', alignItems: 'center', width:'', margin:'0 auto', border:'0px solid red' }}>
+<div id="pastebox" style={{ display: 'flex', flexDirection:'row', gap: '10px', alignItems: 'center', width:'', margin:'', border:'0px solid red' }}>
                             <input
                                 ref={inputElement}
                                 id="youtubelink-input"
@@ -635,7 +637,7 @@ const VideoPlayer = ({ location }) => {
     playsinline
     loop={loop}
     muted={mute} // Set muted prop based on the mute state
-    autoplay={autoplay} // Set autoplay prop based on the autoplay state
+    autoPlay={autoplay} // Change autoplay to autoPlay
     config={{
         youtube: {
             playerVars: { showinfo: false, autoplay: autoplay ? 1 : 0, controls: controls ? 1 : 0, mute: mute ? 1 : 0 } // Set mute and autoplay flags based on state
@@ -661,6 +663,7 @@ const VideoPlayer = ({ location }) => {
         }
     }}
 />
+
 
 
 
