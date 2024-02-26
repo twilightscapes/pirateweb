5465e002e09951ab4c6f2d4a0922c71635b20355
Todd <ihatespam@gmail.com>
2024-02-25 21:05:03 -0600

    DC7

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -545,7 +545,7 @@ const handleAutoplayChange = (event) => {
 
 </div>
 
-                            {!isRunningStandalone() && (
+                            {isRunningStandalone() && (
                             <div style={{position:'absolute', left:'0', top:'50vh', zIndex:'2', display:'flex', flexDirection:'column', alignItems:'center', justifyContent:'center', gap:'2vh', width:'55px',padding:'3px 10px', background:'rgba(0,0,0,.3)', outline:'1px solid #333', borderRadius:'var(--theme-ui-colors-borderRadius)'}}>
 
 
@@ -578,7 +578,7 @@ const handleAutoplayChange = (event) => {
 
 
 
-<div className="font" style={{display:'none', position: 'relative', zIndex: '3', top: '0', width: '100vw', margin: '0 auto', marginTop: showNav ? '0' : '', transition: 'all 1s ease-in-out', 
+<div className="font public" style={{display:'flex', position: 'relative', zIndex: '3', top: '0', width: '100vw', margin: '0 auto', marginTop: showNav ? '0' : '', transition: 'all 1s ease-in-out', 
 // height: hideEditor ? '0' : '50px', 
 // background: 'var(--theme-ui-colors-headerColor)',
  }}>
@@ -643,7 +643,7 @@ background: 'var(--theme-ui-colors-headerColor)',
                     </form>
 
 
-                    {!isRunningStandalone() && (
+                    {isRunningStandalone() && (
                             <div style={{position:'absolute', left:'0', top:'50vh', zIndex:'2', display:'flex', flexDirection:'column', alignItems:'center', justifyContent:'center', gap:'2vh', width:'55px',padding:'3px 10px', background:'rgba(0,0,0,.3)', outline:'1px solid #333', borderRadius:'var(--theme-ui-colors-borderRadius)'}}>
 
 
